f1df42b1e8063860962ef11f86803dcf9f03325f
Caroline Rádlová <caroll.radlova@gmail.com>
2025-05-20 13:49:14 +0200

    final

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,9 @@ import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
 
-import { WorkshopIntro } from '../components/WorkshopIntro.jsx';
-import { Instructor } from '../components/Instructor.jsx';
-import { Venue } from '../components/Venue.jsx';
+import { WorkshopIntro } from '../components/WorkshopIntro/';
+import { Instructor } from '../components/Instructor/';
+import { Venue } from '../components/Venue/';
 
 const response = await fetch('http://localhost:4000/api/workshops/0');
 const data = await response.json();
